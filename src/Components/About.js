07ef8d9c100c70c8b2128521651e5047f20ae15e
CAMERON LUYT <cameronluyt30@gmail.com>
2020-07-09 00:09:10 +0200

    Final Push

---
+++ src/Components/About.js
@@ -0,0 +1,55 @@
+import React, { Component } from 'react';
+
+class About extends Component {
+  render() {
+
+    if(this.props.data){
+      var name = this.props.data.name;
+      var profilepic= "images/"+this.props.data.image;
+      var bio = this.props.data.bio;
+      var street = this.props.data.address.street;
+      var province = this.props.data.address.province;
+      var zip = this.props.data.address.zip;
+      var phone= this.props.data.phone;
+      var email = this.props.data.email;
+    }
+
+    return (
+      <section id="about">
+      <div className="row">
+         
+         <div className="nine columns main-col">
+            <h2>About</h2>
+
+            <p>{bio}</p>
+            <div className="row">
+               <div className="columns contact-details">
+                  <h2>Contact Details</h2>
+                  <p className="address">
+						   {name}<br />
+						   {street}<br />
+						   {province}<br />
+                     {zip}<br />
+						   {phone}<br />
+                     {email}
+					   </p>
+               </div>
+               <div className="columns download">
+                  
+               </div>
+            </div>
+         </div>
+         <div className="three columns">
+            <img className="profile-pic"  src={profilepic} alt="Cameron Jude Luyt Profile Pic" />
+            <p>
+               <a href="CURRICULUM_VITAE_OF_CAMERON_LUYT.docx" className="button"><i className="fa fa-download"></i>Download CV</a>
+            </p>
+         </div>
+      </div>
+
+   </section>
+    );
+  }
+}
+
+export default About;
