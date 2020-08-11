6eb5bc30dfbeca7b760f2e62812f9d9206435167
CAMERON LUYT <cameronluyt30@gmail.com>
2020-08-11 12:24:25 +0200

    Form validation

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -46,12 +46,12 @@ class Contact extends Component {
 
                      <div>
                         <label htmlFor="contactName">Name <span className="required">*</span></label>
-                        <input type="text" defaultValue="" size="35" id="contactName" name="contactName" onChange={this.handleChange}/>
+                        <input type="text" defaultValue="" size="35" id="contactName" name="contactName" onChange={this.handleChange} required/>
                      </div>
 
                      <div>
                         <label htmlFor="contactEmail">Email <span className="required">*</span></label>
-                        <input type="text" defaultValue="" size="35" id="contactEmail" name="contactEmail" onChange={this.handleChange}/>
+                        <input type="text" defaultValue="" size="35" id="contactEmail" name="contactEmail" onChange={this.handleChange} required/>
                      </div>
 
                      <div>
