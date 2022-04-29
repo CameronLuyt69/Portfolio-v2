df9af8a64a754bd8c2c5f0f059ffd34c2d6462cf
Cameron <cameron@securicom.co.za>
2022-04-29 12:57:16 +0200

    Added Docs

---
+++ docs/precache-manifest.c42bf157d15d497e31b48938ef2f36f1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a63fd5606ff07d7a276b8766ffd1ddea",
+    "url": "/Portfolio-v2/index.html"
+  },
+  {
+    "revision": "99824ed5d36394625ca8",
+    "url": "/Portfolio-v2/static/css/main.d498bfb3.chunk.css"
+  },
+  {
+    "revision": "4615e5d6c1243cb6f7fc",
+    "url": "/Portfolio-v2/static/js/2.6941f76d.chunk.js"
+  },
+  {
+    "revision": "f632643c8749d078ad1d2459523cf1a6",
+    "url": "/Portfolio-v2/static/js/2.6941f76d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "99824ed5d36394625ca8",
+    "url": "/Portfolio-v2/static/js/main.a93459f5.chunk.js"
+  },
+  {
+    "revision": "648171c355d6fbe89442",
+    "url": "/Portfolio-v2/static/js/runtime-main.db5d02cd.js"
+  }
+]);
